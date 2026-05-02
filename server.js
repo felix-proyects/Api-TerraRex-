98becbdd0e1a48a7f5dc815d053ac8bb868ce3b8
felix-proyects <frasesbebor@gmail.com>
2026-05-02 14:36:05 -0400

    Actualizar server.js

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ const facebookRoutes = require('./routes/facebookvid');
 const youtubeRoutes = require('./routes/youtube');
 const twitterRoutes = require('./routes/twitter');
 const qrcodeRoutes = require('./routes/qrcode');
-const geminisRoutes = require('./routes/geminis');
-const sswebRoutes = require('./routes/ssweb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/download', express.static(path.join(__dirname, 'public/download')));
 
 app.use('/api/download/tiktok', tiktokRoutes);
 app.use('/api/download/instagram', instagramRoutes);
@@ -25,8 +22,6 @@ app.use('/api/download/facebook', facebookRoutes);
 app.use('/api/download/youtube', youtubeRoutes);
 app.use('/api/download/twitter', twitterRoutes);
 app.use('/api/tools/qr', qrcodeRoutes);
-app.use('/api/ai/gemini', geminisRoutes);
-app.use('/api/tools/ssweb', sswebRoutes);
 
 app.get('/api', (req, res) => {
     res.json({
@@ -40,9 +35,7 @@ app.get('/api', (req, res) => {
             twitter: '/api/download/twitter?url=URL',
             youtube_mp3: '/api/download/youtube/mp3?url=URL',
             youtube_mp4: '/api/download/youtube/mp4?url=URL',
-            qrcode: '/api/tools/qr?text=TEXTO',
-            gemini: '/api/ai/gemini?text=HOLA',
-            ssweb: '/api/tools/ssweb?url=URL'
+            qrcode: '/api/tools/qr?text=TEXTO'
         }
     });
 });
